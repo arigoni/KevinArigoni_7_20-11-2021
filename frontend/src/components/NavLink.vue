599ab1a30add506a2c096966abd1fbd36807f641
Arigoni <arigoni.kevin@yahoo.fr>
2021-12-03 17:10:58 +0100

    Installation de Vue-router

---
+++ frontend/src/components/NavLink.vue
@@ -0,0 +1,16 @@
+<!-- Composant de la partie header avec le logo et le contact -->
+<script>
+    export default {
+	    name: 'HomeLink'
+    }
+</script>
+
+<template>
+	<a href="/">Accueil</a> 
+</template>
+
+<style>
+	a {
+		text-decoration: none;
+	}
+</style>
